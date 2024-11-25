6dab7b4d66690764b5efe991ca5e7b67e0066318
yure-santos <yuricezar.santosanario@gmail.com>
2024-11-24 21:16:05 -0300

    alteração dos icones dos botoes meu diva e buscar profissional
    adicionando os ícones dos botões, icone person e search

--- lib/other_page.dart
+++ lib/other_page.dart
@@ -26,94 +26,97 @@ class _OtherPageState extends State<OtherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.bottomCenter,
-          end: Alignment.center,
-          colors: [
-            AppColors.backgroundColor.withOpacity(0.0),
-            AppColors.backgroundColor.withOpacity(0.5),
-          ],
-        ),
-      ),
-      child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20.0, left: 500, top: 30),
-          child: SizedBox(
-            width: double.infinity,
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/simbolo de psicologia.jpg'),
-              maxRadius: 25,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.center,
+            colors: [
+              AppColors.backgroundColor.withOpacity(0.0),
+              AppColors.backgroundColor.withOpacity(0.5),
+            ],
           ),
         ),
-        Slider(
-          value: _currentValue,
-          onChanged: (newValue) {
-            setState(() {
-              _currentValue = newValue;
-            });
-          },
-          min: 0,
-          max: 1,
-          activeColor: getSliderColor(),
-          inactiveColor: Colors.grey[300],
-        ),
-        Padding(padding: const EdgeInsets.only(top: 50)),
-        SizedBox(
-          width: 300,
-          child: Text('Sua proxima sessão'),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 30, top: 30),
-          child: SizedBox(
-            height: 80,
-            width: 300,
-            child: Card.outlined(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-                side: BorderSide(
-                  color: Color.fromARGB(255, 58, 136, 172),
-                  width: 1,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0, left: 500, top: 30),
+              child: SizedBox(
+                width: double.infinity,
+                child: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/simbolo_de_psicologia.jpg'),
+                  maxRadius: 25,
                 ),
               ),
-              color: Color.fromARGB(255, 58, 136, 172),
-              child: ListTile(
-                textColor: AppColors.appTextDark,
-                title: Text('Não há sessões cadastradas'),
-                subtitle: Text('Configure sua agenda'),
+            ),
+            Slider(
+              value: _currentValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _currentValue = newValue;
+                });
+              },
+              min: 0,
+              max: 1,
+              activeColor: getSliderColor(),
+              inactiveColor: Colors.grey[300],
+            ),
+            Padding(padding: const EdgeInsets.only(top: 50)),
+            SizedBox(
+              width: 300,
+              child: Text('Sua próxima sessão'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30, top: 30),
+              child: SizedBox(
+                height: 80,
+                width: 300,
+                child: Card.outlined(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(
+                      color: Color.fromARGB(255, 58, 136, 172),
+                      width: 1,
+                    ),
+                  ),
+                  color: Color.fromARGB(255, 58, 136, 172),
+                  child: ListTile(
+                    textColor: AppColors.appTextDark,
+                    title: Text('Não há sessões cadastradas'),
+                    subtitle: Text('Configure sua agenda'),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        SizedBox(
-          width: 300,
-          child: Text('Meu terapeuta'),
-        ),
-        SizedBox(
-          height: 80,
-          width: 300,
-          child: Card.outlined(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-              side: BorderSide(
+            SizedBox(
+              width: 300,
+              child: Text('Meu terapeuta'),
+            ),
+            SizedBox(
+              height: 80,
+              width: 300,
+              child: Card.outlined(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  side: BorderSide(
+                    color: AppColors.appTextBlue,
+                    width: 1,
+                  ),
+                ),
                 color: AppColors.appTextBlue,
-                width: 1,
+                child: ListTile(
+                  title: Text('Não há profissional cadastrado'),
+                  subtitle: Text('Adicione um profissional'),
+                ),
               ),
             ),
-            color: AppColors.appTextBlue,
-            child: ListTile(
-              title: Text('Não há profissional cadastrado'),
-              subtitle: Text('Adicione um profissional'),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 40.0),
-          child: Center(
-            child: SizedBox(
-                child: ElevatedButton(
+            // Adicionando os botões
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: [
+                  ElevatedButton.icon(
                     onPressed: () {
                       Navigator.push(
                           context,
@@ -121,23 +124,47 @@ class _OtherPageState extends State<OtherPage> {
                               builder: (context) => const Menu()));
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.loginButton,
+                      backgroundColor: AppColors.appTextBlue,
                       padding:
-                          EdgeInsets.symmetric(horizontal: 70, vertical: 16),
+                          EdgeInsets.symmetric(horizontal: 60, vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                    child: const Text(
+                    icon: Icon(Icons.person, color: Colors.white),
+                    label: Text(
                       'Meu Divã',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ))),
-          ),
+                      style: TextStyle(color: Colors.white, fontSize: 23),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Menu()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.appTextBlue,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    icon: Icon(Icons.search, color: Colors.white),
+                    label: Text(
+                      'Buscar Profissional',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ]),
-    ));
+      ),
+    );
   }
 }
